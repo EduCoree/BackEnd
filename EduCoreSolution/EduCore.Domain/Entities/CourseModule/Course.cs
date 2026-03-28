fa5e8b3dd699a802e8d5ce986a81d8280d14f831
AhmedRady130 <arady6903@gmail.com>
2026-03-28 15:34:34 +0200

    initial

---
+++ EduCoreSolution/EduCore.Domain/Entities/CourseModule/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Domain.Entities.CourseModule
+{
+    public class Course :BaseEntity<int>
+    {
+    }
+}
---
+++ EduCoreSolution/EduCore.Persistencs/Data/DbContexts/EduCoreDbContext.cs
@@ -0,0 +1,31 @@
+﻿using EduCore.Domain.Entities.CourseModule;
+using EduCore.Persistencs.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Persistencs.Data.DbContexts
+{
+    public class EduCoreDbContext : DbContext
+    {
+        public EduCoreDbContext(DbContextOptions<EduCoreDbContext> options) :base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //Access To All Configrations
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CourseConfigurations).Assembly);
+        }
+
+        #region Dbsets
+        public DbSet<Course> courses { get; set; }
+        #endregion
+    }
+}
