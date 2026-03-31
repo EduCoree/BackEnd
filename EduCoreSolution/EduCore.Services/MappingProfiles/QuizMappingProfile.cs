216a44b8e83e13bc3a1464e3cf024aea0aaa5a15
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-03-31 07:20:47 +0200

    Implement QuizService and AutoMapper profile for creating and retrieving quizzes

---
+++ EduCoreSolution/EduCore.Services/MappingProfiles/QuizMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EduCore.Domain.Entities.QuizModel;
+using EduCore.Shared.Dtos.Quiz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Services.MappingProfiles
+{
+    public class QuizMappingProfile:Profile
+    {
+         public QuizMappingProfile()
+         {
+            CreateMap<CreateQuizDto, Quiz>();
+            CreateMap<Quiz, QuizDto>();
+               
+        }
+
+    }
+}
---
+++ EduCoreSolution/EduCore.Services/QuizService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using EduCore.Domain.Contracts;
+using EduCore.Domain.Contracts.Repositories;
+using EduCore.Domain.Entities.CourseModel;
+using EduCore.Domain.Entities.QuizModel;
+using EduCore.Services_Abstraction;
+using EduCore.Shared.Dtos.Quiz;
+using EduCore.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Services
+{
+    public class QuizService:IQuizService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public QuizService(IMapper mapper,IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<QuizDto> CreateQuizAsync(int courseId,CreateQuizDto request)
+        {
+          
+                var courseExists = _unitOfWork.GetRepository<Course, int>().GetByIdAsync(courseId);
+                if (courseExists == null)
+                throw new NotFoundException($"Course with id {courseId} not found.");
+                var quiz = _mapper.Map<Quiz>(request);
+                quiz.CourseId = courseId;
+                await _unitOfWork.QuizRepository.AddAsync(quiz);
+                await _unitOfWork.SaveChangesAsync();
+                return _mapper.Map<QuizDto>(quiz);
+        }
+
+        public async Task<QuizDto> GetQuizByIdAsync(int quizId)
+        {
+            var quiz = await _unitOfWork.QuizRepository.GetQuizWithDetails(quizId);
+            if (quiz == null)
+                throw new NotFoundException ($"Quiz with id {quizId} not found.");
+            return _mapper.Map<QuizDto>(quiz);
+        }
+    }
+}
