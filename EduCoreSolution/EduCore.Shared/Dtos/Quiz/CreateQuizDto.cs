e05cc95030c9044b5531bd26228536ad4d082b85
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-03-31 07:21:09 +0200

    Add Quiz DTOs for create and detail retrieval

---
+++ EduCoreSolution/EduCore.Shared/Dtos/Quiz/CreateQuizDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Shared.Dtos.Quiz
+{
+    public class CreateQuizDto
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; init; } = string.Empty;
+        [Range(1, int.MaxValue, ErrorMessage = "TimeLimitMins must be greater than 0.")]
+        public int? TimeLimitMins { get; init; }
+        [Range(0, 100, ErrorMessage = "PassScore must be between 0 and 100.")]
+        public int PassScore { get; init; }
+        [Range(1, int.MaxValue, ErrorMessage = "MaxAttempts must be greater than 0.")]
+        public int? MaxAttempts { get; init; }
+        public bool IsRandomized { get; init; }
+    }
+}
---
+++ EduCoreSolution/EduCore.Shared/Dtos/Quiz/QuizDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Shared.Dtos.Quiz
+{
+    public class QuizDto
+    {
+        public int Id { get; init; }
+        public int CourseId { get; init; }
+        public string Title { get; init; } = string.Empty;
+        public int? TimeLimitMins { get; init; }
+        public int PassScore { get; init; }
+        public int? MaxAttempts { get; init; }
+        public bool IsRandomized { get; init; }
+    }
+}
