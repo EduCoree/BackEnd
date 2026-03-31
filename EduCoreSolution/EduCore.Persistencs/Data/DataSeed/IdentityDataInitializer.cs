f01a5e37b8fafd6e0568a4002183bc2ab7c74740
HalaMedhatt <halamedhat2486@gmail.com>
2026-03-31 18:52:18 +0200

    feat:result handling

---
+++ EduCoreSolution/EduCore.Persistencs/Data/DataSeed/IdentityDataInitializer.cs
@@ -0,0 +1,61 @@
+﻿using EduCore.Domain.Contracts;
+using EduCore.Domain.Entities.AuthModel;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Persistencs.Data.DataSeed
+{
+    public class IdentityDataInitializer : IDataInitializer
+    {
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public IdentityDataInitializer(UserManager<User> userManager,
+                                        RoleManager<IdentityRole> roleManager) {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                if (!roleManager.Roles.Any())
+                {
+                    await roleManager.CreateAsync(new IdentityRole("Admin"));
+                    await roleManager.CreateAsync(new IdentityRole("Student"));
+                    await roleManager.CreateAsync(new IdentityRole("Teacher"));
+                }
+                if (!userManager.Users.Any())
+                {
+                    var user01 = new User
+                    {
+                        Name = "Hala",
+                        UserName = "HalaMedhat",
+                        Email = "halamedhat2486@gmail.com",
+                        PhoneNumber = "01100775305"
+
+
+                    };
+                    var user02 = new User
+                    {
+                        Name = "Menna",
+                        UserName = "Menna012",
+                        Email = "Menna@gmail.com",
+                        PhoneNumber = "0113245878"
+
+
+                    };
+                    await userManager.CreateAsync(user01);
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ EduCoreSolution/EduCore.Shared/CommonResult/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Shared.CommonResult
+{
+    public class Error
+    {
+        public string Code { get; set; }
+        public string Description { get; set; } 
+        
+
+    }
+}
