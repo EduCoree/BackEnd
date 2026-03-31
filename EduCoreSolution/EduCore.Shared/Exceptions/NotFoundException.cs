6e692eed6ca78eaf923d43d37971984fd3acc522
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-03-31 07:21:37 +0200

    Add Custom NotFound Exception

---
+++ EduCoreSolution/EduCore.Shared/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Shared.Exceptions
+{
+    public class NotFoundException:Exception
+    {
+       
+            public NotFoundException(string message) : base(message)
+            {
+
+            }
+        
+    }
+}
