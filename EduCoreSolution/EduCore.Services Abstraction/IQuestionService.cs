13de71aa6f15da6dc2821ef5f1386bf4e7809a1b
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-04-02 07:36:06 +0200

    feat: Add question Service with validation helper class

---
+++ EduCoreSolution/EduCore.Services Abstraction/IQuestionService.cs
@@ -0,0 +1,17 @@
+﻿using EduCore.Shared.DTOs.Quiz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Services_Abstraction
+{
+    public interface IQuestionService
+    {
+        Task<QuizDetailsDto> GetQuestionsByQuizAsync(int courseId, int quizId);
+        Task<QuestionDto> AddQuestionAsync(int courseId, int quizId,CreateQuestionDto request);
+        Task<QuestionDto> UpdateQuestionAsync(int courseId, int quizId, CreateQuestionDto request);
+        Task DeleteQuestionAsync(int courseId, int quizId, int questionId);
+    }
+}
---
+++ EduCoreSolution/EduCore.Services/Helpers/ValidationHelpers.cs
@@ -0,0 +1,38 @@
+﻿using EduCore.Domain.Contracts;
+using EduCore.Domain.Entities.CourseModel;
+using EduCore.Domain.Entities.QuizModel;
+using EduCore.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Services.Helpers
+{
+    public static class ValidationHelpers
+    {
+
+        public static async Task GetCourseOrThrowAsync(IUnitOfWork unitOfWork, int courseId)
+        {
+            var course = await unitOfWork.GetRepository<Course, int>().AnyAsync(c => c.Id == courseId);
+            if (!course)
+                throw new NotFoundException($"Course with id {courseId} not found.");
+        }
+        public static async Task<Quiz> GetQuizOrThrowAsync(IUnitOfWork unitOfWork, int courseId, int quizId)
+        {
+            var quiz = await unitOfWork.QuizRepository.GetByIdAsync(quizId);
+            if (quiz is null || quiz.CourseId != courseId)
+                throw new NotFoundException($"Quiz with id {quizId} not found in course {courseId}.");
+            return quiz;
+        }
+
+        public static async Task<Question> GetQuestionOrThrowAsync(IUnitOfWork unitOfWork, int quizId, int questionId)
+        {
+            var question = await unitOfWork.GetRepository<Question, int>().GetByIdAsync(questionId);
+            if (question is null || question.QuizId != quizId)
+                throw new NotFoundException($"Question with id {questionId} not found in quiz {quizId}.");
+            return question;
+        }
+    }
+}
