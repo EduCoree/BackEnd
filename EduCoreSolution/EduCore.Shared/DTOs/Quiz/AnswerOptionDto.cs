61f6c8d611e2e61f23ad5534fc0c0f97ef7f546a
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-04-03 06:21:50 +0200

    Chore: updating some files

--- EduCoreSolution/EduCore.Shared/DTOs/Quiz/AnswerOptionDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EduCore.Shared.DTOs.Quiz
-{
-    public class AnswerOptionDto
-    {
-        public int Id { get; init; }
-        public string Text { get; init; } = string.Empty;
-        public bool IsCorrect { get; init; } 
-    }
-}
