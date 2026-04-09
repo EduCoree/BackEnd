cc467d4ef72476a3eb59631b4268c083e84e50aa
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-04-09 16:22:49 +0200

    feat: Added EnrollmentRepository to verify student-course ownership.

---
+++ EduCoreSolution/EduCore.Persistencs/Repositories/EnrollmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using EduCore.Domain.Contracts.Repositories;
+using EduCore.Domain.Entities.EnrollmentModel;
+using EduCore.Persistencs.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Persistencs.Repositories
+{
+    public class EnrollmentRepository : GenericRepository<Enrollment, int>, IEnrollmentRepository
+    {
+        public EnrollmentRepository(EduCoreDbContext context) : base(context)
+        {
+        }
+        public async Task<bool> IsEnrolledAsync(string studentId, int courseId)
+        {
+            return await _EduCoreDbContext.Enrollments.AnyAsync(e=>e.StudentId==studentId && e.CourseId==courseId);
+        }
+    }
+}
