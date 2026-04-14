16ecc5a99b5320e549f5d15b7a1fe244a54b9354
Ahmed-Badr-El-Din <conix2525722@gmail.com>
2026-04-14 04:52:15 +0200

    added it to IUnitOfWork

--- EduCoreSolution/EduCore.Domain/Contracts/IUnitOfWork.cs
+++ EduCoreSolution/EduCore.Domain/Contracts/IUnitOfWork.cs
@@ -28,5 +28,8 @@ namespace EduCore.Domain.Contracts
 
         // ── Forum ──
         IForumRepository ForumRepository { get; }
+
+        // ── Chat ──
+        IChatRepository ChatRepository { get; }
     }
 }
