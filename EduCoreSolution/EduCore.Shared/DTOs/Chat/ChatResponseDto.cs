fa43282d1272cbc8fb9f1ee46e8f833775f47e5c
Ahmed-Badr-El-Din <conix2525722@gmail.com>
2026-04-14 04:57:15 +0200

    Chat API DTOs

---
+++ EduCoreSolution/EduCore.Shared/DTOs/Chat/ChatResponseDto.cs
@@ -0,0 +1,8 @@
+namespace EduCore.Shared.DTOs.Chat
+{
+    public class ChatResponseDto
+    {
+        public string Reply { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
