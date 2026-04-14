37fb05797d6fe59a786b71d04752fed711f5a4a8
Ahmed-Badr-El-Din <conix2525722@gmail.com>
2026-04-14 19:26:10 +0200

    add migration for forum posts lesson-level FK

--- EduCoreSolution/EduCore.Persistencs/Data/Migrations/20260414172101_ForumPostLessonLevel.Designer.cs
+++ EduCoreSolution/EduCore.Persistencs/Data/Migrations/20260414172101_ForumPostLessonLevel.Designer.cs
@@ -4,6 +4,7 @@ using EduCore.Persistencs.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EduCore.Persistencs.Data.Migrations
 {
     [DbContext(typeof(EduCoreDbContext))]
-    partial class EduCoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260414172101_ForumPostLessonLevel")]
+    partial class ForumPostLessonLevel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
