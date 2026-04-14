c9da422d8efa3ca3bd68129dd2e7e4c76ca42799
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-04-14 15:55:28 +0200

    feat/adding notification mapping profile

---
+++ EduCoreSolution/EduCore.Services/MappingProfiles/NotificationMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EduCore.Domain.Entities.NotificationsModel;
+using EduCore.Shared.DTOs.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCore.Services.MappingProfiles
+{
+    public class NotificationMappingProfile:Profile
+    {
+        public NotificationMappingProfile()
+        {
+            CreateMap<Notification, NotificationDto>();
+        }
+
+    }
+}
