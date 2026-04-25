06a994f0e0d6bdb1ee51896f1ed248140c05645c
AhmedRady130 <arady6903@gmail.com>
2026-04-25 14:53:27 +0300

    create cash payment request

--- EduCoreSolution/EduCore.Shared/Enums/EnrollEnums.cs
+++ EduCoreSolution/EduCore.Shared/Enums/EnrollEnums.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace EduCore.Shared.Enums
 {
+
+    public enum CashRequestStatus
+    {
+        Pending,
+        Confirmed,
+        Rejected
+    }
     public enum PaymentMethod { CreditCard, PayPal, BankTransfer, Stripe, Fawry,Paymob }
     public enum PaymentStatus { Pending, Completed, Failed, Refunded }
 }
