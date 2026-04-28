5a97f8fc6c6362c9376bcfa1a7f6ca132be03ee8
AhmedRady130 <arady6903@gmail.com>
2026-04-28 19:35:06 +0300

    publish on vercel

--- EduCoreSolution/EduCore/Program.cs
+++ EduCoreSolution/EduCore/Program.cs
@@ -363,9 +363,17 @@ namespace EduCore
             builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
             builder.Services.AddScoped<IPaymentService, PaymentService>();
             builder.Services.AddScoped<ICashPaymentRequestService, CashPaymentRequestService>();
-            //        builder.Services.AddControllers()
-            //.AddApplicationPart(
-            //    typeof(EduCore.Presentation.Controllers.AdminCoursesController).Assembly);
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend",
+                    policy =>
+                    {
+                        policy
+                            .WithOrigins("https://front-end-6u71.vercel.app")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
             builder.Services.AddScoped<ICategoryService, CategoryService>();
             var keysPath = System.IO.Path.Combine(builder.Environment.ContentRootPath, "keys");
             builder.Services.AddDataProtection()
@@ -458,11 +466,12 @@ namespace EduCore
             });
 
             app.UseHttpsRedirection();
-            app.UseCors("AllowAngular");
-            app.UseStaticFiles();
+            //app.UseCors("AllowAngular");
+            app.UseRouting();
+            app.UseCors("AllowFrontend");
             app.UseAuthentication();
             app.UseAuthorization();
-            //D:\GraduationProject_ITI\BackEnd\EduCoreSolution\EduCore.Presentation\Hubs\
+            app.UseStaticFiles();
             app.MapHub<NotificationHub>("/hubs/notifications");
 
 
